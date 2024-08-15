0f10aa39258d8e251ce762967fd7de5853ae4b5e
diepdp <diepdp@kaiyouit.com>
2024-08-15 10:51:53 +0700

    re deploy

--- app/firebaseApp.ts
+++ app/firebaseApp.ts
@@ -6,14 +6,14 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY ?? 'AIzaSyAQLlUzpdH_j0Ouy0OFaDV-pRFRdootC2Q',
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN ?? 'anime-website-8c919.firebaseapp.com',
+    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL ?? 'https://anime-website-8c919-default-rtdb.asia-southeast1.firebasedatabase.app',
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID ?? 'anime-website-8c919',
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET ?? 'anime-website-8c919.appspot.com',
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER ?? '765187117478',
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID ?? '1:765187117478:web:9f23c7747a32ea8ea0bf67',
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID ?? 'G-GNRS4BPHHG'
 };
 
 // Initialize Firebase
