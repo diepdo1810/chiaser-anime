6a52252c8171865bf8dfd16673f1e563b35144a9
diepdp <diepdp@kaiyouit.com>
2024-09-10 16:26:06 +0700

    hotfix: pagination and display showing

--- app/ocategory/[slug]/components/MangaList/index.tsx
+++ app/ocategory/[slug]/components/MangaList/index.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import React, { use, useEffect, useState } from 'react';
-import omanga from '@/app/api/oManga'; // Update with correct path
-import { MangaCategoryResponse } from '@/app/ts/interfaces/apiOMangaDataInterface'; // Update with correct path
-import styles from './component.module.css'; 
-import { motion, AnimatePresence } from 'framer-motion';
-import * as MediaCard from '@/app/components/MediaCards/MediaCard'
-import BreadCrumb from '../BreadCrumb';
+import React, { useEffect, useState } from "react";
+import omanga from "@/app/api/oManga";
+import { MangaCategoryResponse } from "@/app/ts/interfaces/apiOMangaDataInterface";
+import styles from "./component.module.css";
+import { motion, AnimatePresence } from "framer-motion";
+import * as MediaCard from "@/app/components/MediaCards/MediaCard";
+import BreadCrumb from "../BreadCrumb";
 
 interface MangaListProps {
   slug: string;
@@ -14,149 +14,176 @@ interface MangaListProps {
 }
 
 const framerMotionShowUp = {
-
-    hidden: {
-        opacity: 0,
-        scale: 1.08
+  hidden: {
+    opacity: 0,
+    scale: 1.08,
+  },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      delayChildren: 0.5,
     },
-    visible: {
-        opacity: 1,
-        scale: 1,
-        transition: {
-            delayChildren: 0.5
-        }
-    }
-
-}
+  },
+};
 
 const MangaList: React.FC<MangaListProps> = ({ slug, page = 1 }) => {
-  const [comics, setComics] = useState<MangaCategoryResponse['data']['items']>([]);
+  const [comics, setComics] = useState<MangaCategoryResponse["data"]["items"]>(
+    []
+  );
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
   const [totalLength, setTotalLength] = useState(0);
   const [currPageNumber, setCurrPageNumber] = useState(page);
-  const [showing, setShowing] = useState<string>('');
+  const [totalPages, setTotalPages] = useState(0);
   const [items, setItems] = useState([]);
-  const defaultComics = '24';
+  const defaultComics = "24";
 
   useEffect(() => {
     const fetchComics = async () => {
-        setLoading(true);
-        try {
-            const response = await omanga.getComicsByCategory({ slug, page: currPageNumber });
-            if (response && response.data) {
-                setItems(response.data);
-                setComics((prevComics) => currPageNumber === 1 
-                    ? response.data.items 
-                    : [...(prevComics || []), ...response.data.items]);
-                setTotalLength(response.data.params.pagination.totalItems);
-                setShowing(comics.length === 0 ? `${defaultComics}` : `${comics.length}`);
-            } else {
-                setError('No comics found.');
-            }
-        } catch (err) {
-            setError('An error occurred while fetching comics.');
-        } finally {
-            setLoading(false);
+      setLoading(true);
+      try {
+        const response = await omanga.getComicsByCategory({
+          slug,
+          page: currPageNumber,
+        });
+        if (response && response.data) {
+          setItems(response.data);
+          setComics((prevComics) =>
+            currPageNumber === 1
+              ? response.data.items
+              : [...(prevComics || []), ...response.data.items]
+          );
+          setTotalLength(response.data.params.pagination.totalItems);
+          setTotalPages(
+            Math.ceil(
+              response.data.params.pagination.totalItems /
+                response.data.params.pagination.totalItemsPerPage
+            )
+          );
+        } else {
+          setError("No comics found.");
         }
+      } catch (err) {
+        setError("An error occurred while fetching comics.");
+      } finally {
+        setLoading(false);
+      }
     };
 
     fetchComics();
-
   }, [slug, currPageNumber]);
 
   const fetchNextResultPage = async () => {
-        const nextPage = currPageNumber + 1;
+    const nextPage = currPageNumber + 1;
 
-        try {
-            setLoading(true);
+    try {
+      setLoading(true);
 
-            const response = await omanga.getComicsByCategory({ slug, page: nextPage });
+      const response = await omanga.getComicsByCategory({
+        slug,
+        page: nextPage,
+      });
 
-            if (response && response.data) {
-                setComics((prevComics) => prevComics ? [...prevComics, ...response.data.items] : response.data.items);
-                setCurrPageNumber(nextPage);
-            } else {
-                setError('No more comics found.');
-            }
-        } catch (err) {
-            setError('An error occurred while fetching more comics.');
-        } finally {
-            setLoading(false);
-        }
-    };
+      if (response && response.data) {
+        setComics((prevComics) =>
+          prevComics
+            ? [...prevComics, ...response.data.items]
+            : response.data.items
+        );
+        setCurrPageNumber(nextPage);
+      } else {
+        setError("No more comics found.");
+      }
+    } catch (err) {
+      setError("An error occurred while fetching more comics.");
+    } finally {
+      setLoading(false);
+    }
+  };
 
-  if (loading  && currPageNumber === 1) return <p>Loading...</p>;
+  if (loading && currPageNumber === 1) return <p>Loading...</p>;
   if (error) return <p>{error}</p>;
 
   return (
     <div id={styles.content_container}>
-        <BreadCrumb items={items.breadCrumb} />
-        <div id={styles.heading_container}></div>
+      <BreadCrumb items={items.breadCrumb} />
+      <div id={styles.heading_container}></div>
 
-        {comics && comics.length === 0 && (
+      {comics && comics.length === 0 && (
         <div id={styles.error_text}>
-            <h2>No Results Found</h2>
+          <h2>No Results Found</h2>
         </div>
-        )}
+      )}
 
-        {comics && comics.length > 0 && (
-            <motion.div>
+      {comics && comics.length > 0 && (
+        <motion.div>
+          <AnimatePresence initial={true} mode="wait">
+            <motion.ul
+              id={styles.results_container}
+              initial="hidden"
+              animate="visible"
+              variants={framerMotionShowUp}
+            >
+              {comics
+                .filter(
+                  (comic, index, self) =>
+                    index === self.findIndex((c) => c._id === comic._id)
+                )
+                .map((comic, key) => (
+                  <motion.li key={comic._id} variants={framerMotionShowUp}>
+                    <MediaCard.Container onDarkMode>
+                      <MediaCard.MediaImgLinkOmanga
+                        hideOptionsButton
+                        mediaInfo={comic as any}
+                        title={comic.name}
+                        formatOrType="MANGA"
+                        url={omanga.getImageUrl(comic.thumb_url)}
+                        mediaId={comic.slug}
+                      />
 
-                <AnimatePresence initial={true} mode="wait">
-                    <motion.ul id={styles.results_container} initial="hidden" animate="visible" variants={framerMotionShowUp} >
-                        {comics.map((comic, key) => (
-                        <motion.li key={comic._id} variants={framerMotionShowUp}>
-                            <MediaCard.Container onDarkMode>
-                                <MediaCard.MediaImgLinkOmanga
-                                    hideOptionsButton
-                                    mediaInfo={comic as any}
-                                    title={comic.name}
-                                    formatOrType="MANGA"
-                                    url={omanga.getImageUrl(comic.thumb_url)}
-                                    mediaId={comic.slug}
-                                />
+                      <MediaCard.SmallTag
+                        seasonYear={new Date(comic.updatedAt).getFullYear()}
+                        tags={comic.status}
+                      />
 
-                                <MediaCard.SmallTag
-                                    seasonYear={new Date(comic.updatedAt).getFullYear()}
-                                    tags={comic.status}
-                                />
+                      <MediaCard.LinkTitleOmanga title={comic.name} />
+                    </MediaCard.Container>
+                  </motion.li>
+                ))}
+            </motion.ul>
+          </AnimatePresence>
+        </motion.div>
+      )}
 
-                                <MediaCard.LinkTitleOmanga
-                                    title={comic.name}
-                                />
-
-                            </MediaCard.Container>
-                        </motion.li>
-                        ))}
-                    </motion.ul>
-                </AnimatePresence>
-            </motion.div>
-        )}
-
-        {comics && totalLength > comics.length * currPageNumber && (
+      {comics && currPageNumber < totalPages && (
         <button
-            onClick={() => fetchNextResultPage()}
-            aria-label={loading ? 'Loading' : 'View More Results'}
+          onClick={() => fetchNextResultPage()}
+          aria-label={loading ? "Loading" : "View More Results"}
         >
-            {loading ? 'Loading...' : ' + View more'}
+          {loading ? "Loading..." : " + View more"}
         </button>
-        )}
+      )}
 
-        {comics && comics.length > 0 && (
-            <span>
-                Showing {(totalLength <= comics.length * currPageNumber) ? 'all ' : showing + ' out of '}
-                <span>{totalLength.toLocaleString('en-US')}</span> results
-            </span>
-        )}
-
-        <span style={{fontSize: 'var(--font-size--small-2)' }}>
-            Last Update: {new Date().toLocaleDateString('en-us', { day: 'numeric', month: 'long', year: 'numeric' })}
+      {comics && comics.length > 0 && (
+        <span>
+          Showing{" "}
+          {Math.min(currPageNumber * 24, totalLength) === totalLength
+            ? "all "
+            : `${Math.min(currPageNumber * 24, totalLength)} out of `}
+          <span>{totalLength.toLocaleString("en-US")}</span> results
         </span>
+      )}
 
-
-  </div>
-
+      <span style={{ fontSize: "var(--font-size--small-2)" }}>
+        Last Update:
+        {new Date().toLocaleDateString("en-us", {
+          day: "numeric",
+          month: "long",
+          year: "numeric",
+        })}
+      </span>
+    </div>
   );
 };
 
